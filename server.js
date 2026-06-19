2a1cb585baa7bf13a676423e5a0d11be09c93dd7
annacarlaferrero-ai <annacarla.ferrero@gmail.com>
2026-06-19 15:07:57 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+{
+  "name": "suivi-st-floriot",
+  "version": "1.0.0",
+  "description": "Suivi financier des sous-traitants - Floriot",
+  "main": "server.js",
+  "scripts": { "start": "node server.js" },
+  "dependencies": {
+    "express": "^4.18.2",
+    "better-sqlite3": "^9.4.3"
+  }
+}
